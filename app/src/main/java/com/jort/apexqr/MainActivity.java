50aa487ba27e36ad0f1d6f8a6ad73a4c5ee7e8fb
Ferdinand Escano <the.jort@gmail.com>
2020-11-21 17:24:15 +0800

    Layout completed

---
+++ app/src/main/java/com/jort/apexqr/MainActivity.java
@@ -0,0 +1,54 @@
+package com.jort.apexqr;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView cv_scan, cv_logs, cv_data, cv_setup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cv_scan = findViewById(R.id.cv_scan);
+        cv_logs = findViewById(R.id.cv_logs);
+        cv_data = findViewById(R.id.cv_data);
+        cv_setup = findViewById(R.id.cv_setup);
+
+        cv_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "SCAN", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cv_logs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "LOGS", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cv_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "DATA", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cv_setup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "SETUP", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
